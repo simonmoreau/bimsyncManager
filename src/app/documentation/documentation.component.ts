8d47284623fd99a88c5859a2a2a19ef22a1eaa27
Simon Moreau <simon@bim42.com>
2018-01-16 22:34:31 +0100

    Set up documentation page

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'app/app.service';
+import * as prismjs from 'prismjs';
 
 @Component({
   selector: 'app-documentation',
@@ -14,7 +15,67 @@ export class DocumentationComponent implements OnInit {
       this.appService = _appService;
   }
 
+  private code = `
+  [
+    {
+      "name":"En cours",
+      "color":"#e69138",
+      "type":"candidate"
+    },
+    {
+      ...
+    }
+  ]`;
+
+  private code1 =`
+  [
+    {
+        "projectName": “The name of your new project",
+        "projectDescription": “The description of your new project",
+        "users”:[ … ]
+        "models”:[ ... ] 
+        "boards”: [ … ] 
+    },
+    {
+        "projectName": “...",
+        "projectDescription": “The description of your new project”,
+        ...
+    }
+  ]`;
+
+  private code2 =`
+  {
+      "id”:”the Id of the new member",
+      "role":"administrator"
+  }`;
+
+  private code3 =`
+  {
+    “name”:”The name of your model"
+  }`;
+
+  private code4 =`
+  {
+    "name":"Questions de conception",
+    "statuses”: [ … ],
+    “types”: [ … ]
+}`;
+
+private code5=`
+{
+  "name":"En cours",
+  "color":"#e69138",
+  "type":"candidate"
+}`;
+
+private code6=`
+{
+  "name": "Avertissement",
+  "color": "#e69138"
+},`;
+
   ngOnInit() {
+    
   }
 
 }
