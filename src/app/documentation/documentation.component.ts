177e1adb793e77f7911dfa2147a08cb8dfef2075
Simon Moreau <simon@bim42.com>
2018-01-18 19:48:23 +0100

    Fine tune documentation

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -9,10 +9,10 @@ import * as prismjs from 'prismjs';
 })
 export class DocumentationComponent implements OnInit {
 
-  appService:AppService;
+  appService: AppService;
 
   constructor(private _appService: AppService) {
-      this.appService = _appService;
+    this.appService = _appService;
   }
 
   private code = `
@@ -27,55 +27,150 @@ export class DocumentationComponent implements OnInit {
     }
   ]`;
 
-  private code1 =`
+  private code1 = `
   [
     {
         "projectName": “The name of your new project",
         "projectDescription": “The description of your new project",
-        "users”:[ … ]
-        "models”:[ ... ] 
-        "boards”: [ … ] 
+        "users":[ … ]
+        "models":[ ... ] 
+        "boards": [ … ] 
     },
     {
         "projectName": “...",
-        "projectDescription": “The description of your new project”,
+        "projectDescription": “The description of your new project",
         ...
     }
   ]`;
 
-  private code2 =`
+  private code2 = `
   {
-      "id”:”the Id of the new member",
+      "id":"the Id of the new member",
       "role":"administrator"
   }`;
 
-  private code3 =`
+  private code3 = `
   {
-    “name”:”The name of your model"
+      “name":"The name of your model"
   }`;
 
-  private code4 =`
+  private code4 = `
   {
-    "name":"Questions de conception",
-    "statuses”: [ … ],
-    “types”: [ … ]
-}`;
+      "name":"My new Issue Board",
+      "statuses": [ … ],
+      "types": [ … ]
+  }`;
 
-private code5=`
-{
-  "name":"En cours",
-  "color":"#e69138",
-  "type":"candidate"
-}`;
+  private code5 = `
+  {
+      "name":"Current",
+      "color":"#e69138",
+      "type":"candidate"
+  }`;
 
-private code6=`
-{
-  "name": "Avertissement",
-  "color": "#e69138"
-},`;
+  private code6 = `
+  {
+      "name": "Warning",
+      "color": "#e69138"
+  },`;
+
+  private example = `
+  [
+    {
+        "projectName": "00000 - My New Project",
+        "projectDescription": "My new project description",
+        "users":
+        [
+            {
+                "id":"userid1",
+                "role":"administrator"
+            },
+            {
+                "id":"userid2",
+                "role":"member"
+            },
+            {
+                "id":"userid3",
+                "role":"member"
+            }
+        ],
+        "models":
+        [
+            {
+                "name":"Architect"
+            },
+            {
+                "name":"Structure"
+            },
+            {
+                "name":"HVAC"
+            }           
+        ],
+        "boards":
+        [
+            {
+                "name":"My New Issue Board",
+                "statuses":
+                [
+                    {
+                        "name":"New",
+                        "color":"#dd7e6b",
+                        "type":"open"
+                    },
+                    {
+                        "name":"Current",
+                        "color":"#e69138",
+                        "type":"candidate"
+                    },
+                    {
+                        "name":"Closed",
+                        "color":"#6aa84f",
+                        "type":"closed"
+                    }
+                ],
+                "types":
+                [
+                    {
+                        "name": "Error",
+                        "color": "#cc0000"
+                    },
+                    {
+                        "name": "Warning",
+                        "color": "#e69138"
+                    }
+                ]
+            },
+            {
+                "name":"My Second Issue Board",
+                "statuses":
+                [
+                    {
+                        "name":"New",
+                        "color":"#dd7e6b",
+                        "type":"open"
+                    },
+                    {
+                        "name":"Current",
+                        "color":"#e69138",
+                        "type":"candidate"
+                    },
+                    {
+                        "name":"Closed",
+                        "color":"#6aa84f",
+                        "type":"closed"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "projectName": "00000 - My Other New Project",
+        "projectDescription": "My new project description"
+    }
+]`;
 
   ngOnInit() {
-    
+
   }
 
 }
