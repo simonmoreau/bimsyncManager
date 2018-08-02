249005c3f9b2a87f25f1f63ddcdf7d02d908e9d2
simonmoreau <simon@bim42.com>
2018-08-02 15:04:41 +0200

    Update to Angular 5.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import {HttpClientModule} from '@angular/common/http';
 import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
 import { HomeComponent } from "./home/home.component";
 import { BimsyncProjectComponent } from './bimsync-project/bimsync-project.component';
-import { HttpClient } from "@angular/common/http/src/client";
 import { BimsyncOauthComponent } from './bimsync-oauth/bimsync-oauth.component';
 import { DocumentationComponent } from './documentation/documentation.component';
 import { TakeoffComponent } from './takeoff/takeoff.component';
@@ -29,7 +27,6 @@ import { ShareModalComponent } from './share-modal/share-modal.component';
         BrowserAnimationsModule,
         BrowserModule,
         FormsModule,
-        HttpModule,
         HttpClientModule,
         ClarityModule,
         ROUTING
