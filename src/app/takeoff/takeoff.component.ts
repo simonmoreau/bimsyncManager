46c3d976afbcc67a33172ec1a621497ccb77d392
Simon Moreau <simon@bim42.com>
2018-10-04 20:46:05 +0200

    Add a spinner

--- src/app/takeoff/takeoff.component.ts
+++ src/app/takeoff/takeoff.component.ts
@@ -1,106 +1,134 @@
-import { Component, OnInit } from '@angular/core';
-import { TakeoffService } from './takeoff.services';
-import { IProject, IModel, IRevision, IProduct } from '../bimsync-project/bimsync-project.models';
-import { ITypeSummary } from './takeoff.model';
+import { Component, OnInit } from "@angular/core";
+import { TakeoffService } from "./takeoff.services";
+import {
+    IProject,
+    IModel,
+    IRevision,
+    IProduct
+} from "../bimsync-project/bimsync-project.models";
+import { ITypeSummary } from "./takeoff.model";
 
 @Component({
-  selector: 'app-takeoff',
-  templateUrl: './takeoff.component.html',
-  styleUrls: ['./takeoff.component.scss'],
-  providers: [TakeoffService]
+    selector: "app-takeoff",
+    templateUrl: "./takeoff.component.html",
+    styleUrls: ["./takeoff.component.scss"],
+    providers: [TakeoffService]
 })
 export class TakeoffComponent implements OnInit {
+    projects: IProject[] = [];
+    selectedProject: IProject;
+    models: IModel[] = [];
+    revisions: IRevision[] = [];
+    selectedModel: IModel;
+    selectedRevision: IRevision;
+    errorMessage: string;
+    ifcClasses: ITypeSummary[] = [];
+    selectedIfcClass: ITypeSummary;
+    selectedElements: IProduct[] = [];
+    selectedElementsLoading: boolean = false;
 
-  projects: IProject[] = [];
-  selectedProject: IProject;
-  models: IModel[] = [];
-  revisions: IRevision[] = [];
-  selectedModel: IModel;
-  selectedRevision: IRevision;
-  errorMessage: string;
-  typesSummary: ITypeSummary[] = [];
-  selectedIfcClass: ITypeSummary;
-  selectedElements: IProduct[] = [];
+    constructor(private _takeoffService: TakeoffService) {}
 
-  constructor(private _takeoffService: TakeoffService) { }
+    ngOnInit() {
+        this.GetProjects();
+    }
 
-  ngOnInit() {
-    this.GetProjects();
-  }
-
-  GetProjects() {
-    this._takeoffService.getProjects()
-      .subscribe(projects => {
-        this.projects = projects;
-      },
-        error => this.errorMessage = <any>error);
-    return false;
-  }
-
-  GetModels() {
-    this._takeoffService.getModels(this.selectedProject.id)
-      .subscribe(models => {
-        this.models = models;
-        if (this.models != null && this.models.length !== 0) {
-          this.selectedModel = this.models[0];
-          this.GetRevisions();
-        } else {
-          this.selectedModel = null;
-        }
-      },
-        error => this.errorMessage = <any>error);
-    return false;
-  }
-
-  GetRevisions() {
-    this._takeoffService.getRevisions(this.selectedProject.id, this.selectedModel.id)
-      .subscribe(revisions => {
-        this.revisions = revisions;
-        if (this.revisions != null && this.revisions.length !== 0) {
-          this.selectedRevision = this.revisions[0];
-          this.GetProductTypeSummary();
-        } else {
-          this.selectedRevision = null;
-        }
-      },
-        error => this.errorMessage = <any>error);
-    return false;
-  }
-
-  GetProductTypeSummary() {
-    this._takeoffService.getProductsTypeSummary(this.selectedProject.id, this.selectedRevision.id)
-      .subscribe(summaryData => {
-        this.typesSummary.length = 0;
-        Object.keys(summaryData).forEach(
-          key => {
-
-            let summary: ITypeSummary = {
-              typeName: key,
-              typeQuantity: summaryData[key]
-            };
-
-            this.typesSummary.push(summary);
-          }
+    GetProjects() {
+        this._takeoffService.getProjects().subscribe(
+            projects => {
+                this.projects = projects;
+            },
+            error => (this.errorMessage = <any>error)
         );
+        return false;
+    }
+
+    GetModels() {
+        this._takeoffService.getModels(this.selectedProject.id).subscribe(
+            models => {
+                this.models = models;
+                if (this.models != null && this.models.length !== 0) {
+                    this.selectedModel = this.models[0];
+                    this.GetRevisions();
+                } else {
+                    this.selectedModel = null;
+                }
+            },
+            error => (this.errorMessage = <any>error)
+        );
+        return false;
+    }
+
+    GetRevisions() {
+        this._takeoffService
+            .getRevisions(this.selectedProject.id, this.selectedModel.id)
+            .subscribe(
+                revisions => {
+                    this.revisions = revisions;
+                    if (this.revisions != null && this.revisions.length !== 0) {
+                        this.selectedRevision = this.revisions[0];
+                        this.GetProductTypeSummary();
+                    } else {
+                        this.selectedRevision = null;
+                    }
+                },
+                error => (this.errorMessage = <any>error)
+            );
+        return false;
+    }
+
+    GetProductTypeSummary() {
+        this._takeoffService
+            .getProductsTypeSummary(
+                this.selectedProject.id,
+                this.selectedRevision.id
+            )
+            .subscribe(
+                summaryData => {
+                    this.ifcClasses.length = 0;
+                    Object.keys(summaryData).forEach(key => {
+                        let summary: ITypeSummary = {
+                            typeName: key,
+                            typeQuantity: summaryData[key]
+                        };
+
+                        this.ifcClasses.push(summary);
+                    });
+                    this.selectedIfcClass = this.ifcClasses.filter(function(x) {
+                        return x.typeName === "IfcProject";
+                    })[0];
+
+                    this.GetProducts();
+                },
+                error => (this.errorMessage = <any>error)
+            );
+        return false;
+    }
+
+    GetProducts() {
         this.selectedElements.length = 0;
-        this.GetProducts();
-      },
-        error => this.errorMessage = <any>error);
-    return false;
-  }
+        this.selectedElementsLoading = true;
+        // It will loop on all requests
+        this._takeoffService
+            .getProducts(
+                this.selectedProject.id,
+                this.selectedRevision.id,
+                this.selectedIfcClass.typeName,
+                this.selectedIfcClass.typeQuantity
+            )
+            .subscribe(
+                products => {
+                    this.selectedElements = this.selectedElements.concat(
+                        products
+                    );
+                    this.selectedElementsLoading = false;
+                },
+                error => (this.errorMessage = <any>error)
+            );
+        return false;
+    }
 
-  GetProducts() {
-    this.selectedElements.length = 0;
-    //It will loop on all requests
-    this._takeoffService.getProducts(this.selectedProject.id, this.selectedRevision.id, this.selectedIfcClass.typeName, this.selectedIfcClass.typeQuantity)
-      .subscribe(products => {
-        this.selectedElements = this.selectedElements.concat(products);
-      },
-        error => this.errorMessage = <any>error);
-    return false;
-  }
-
-  trackByFn(index, model) {
-    return model.id;
-  }
+    trackByFn(index, model) {
+        return model.id;
+    }
 }
