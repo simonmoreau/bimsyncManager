5931b4042329615ba22fca7fb51ad82d3b618277
simonmoreau <simon@bim42.com>
2018-11-29 13:51:59 +0100

    small fixes

--- src/app/takeoff/takeoff.component.ts
+++ src/app/takeoff/takeoff.component.ts
@@ -9,7 +9,7 @@ import {
 } from "../bimsync-project/bimsync-project.models";
 import {
     ITypeSummary, IProduct, IPropertySet, IProperty,
-    IQuantitySet, DisplayProperty,
+    IQuantitySet, DisplayProperty, GroupingModeEnum,
     IDisplayPropertySet, Products, ValueTree, Guid, IHighlightedElements, SortEnum, IQuantity
 } from "./takeoff.model";
 import { DropEvent } from 'ng-drag-drop';
@@ -64,7 +64,7 @@ export class TakeoffComponent implements OnInit {
 
     GetModels() {
         this.selectedValueProperties.length = 0;
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.highlightedElements = [];
 
         this._takeoffService.getModels(this.selectedProject.id).subscribe(
@@ -84,7 +84,7 @@ export class TakeoffComponent implements OnInit {
 
     GetRevisions() {
         this.selectedValueProperties.length = 0;
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.highlightedElements = [];
 
         this._takeoffService
@@ -106,7 +106,7 @@ export class TakeoffComponent implements OnInit {
 
     GetProductTypeSummary() {
         this.selectedValueProperties.length = 0;
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.highlightedElements = [];
         this.viewer3dToken = null;
 
@@ -122,8 +122,10 @@ export class TakeoffComponent implements OnInit {
                     this.ifcClasses.length = 0;
 
                     //Merge walls in summary
-                    summaryData['IfcWall'] = summaryData['IfcWall'] + summaryData['IfcWallStandardCase'];
-                    delete summaryData['IfcWallStandardCase'];
+                    if (summaryData['IfcWall'] != null) {
+                        summaryData['IfcWall'] = summaryData['IfcWall'] + summaryData['IfcWallStandardCase'];
+                        delete summaryData['IfcWallStandardCase'];
+                    }
 
                     Object.keys(summaryData).forEach(key => {
                         let summary: ITypeSummary = {
@@ -134,7 +136,14 @@ export class TakeoffComponent implements OnInit {
                         this.ifcClasses.push(summary);
                     });
 
-
+                    //Sort ifcClasees
+                    this.ifcClasses = this.ifcClasses.sort((a, b): number => {
+                        if (a.typeName < b.typeName)
+                            return -1;
+                        if (a.typeName > b.typeName)
+                            return 1;
+                        return 0;
+                    });
 
                     this.selectedIfcClass = this.ifcClasses.filter(function (x) {
                         return x.typeName === "IfcProject";
@@ -181,7 +190,7 @@ export class TakeoffComponent implements OnInit {
 
     GetProducts() {
         this.selectedValueProperties.length = 0;
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.selectedProducts.length = 0;
         this.selectedProductsLoading = true;
         this.highlightedElements = [];
@@ -204,7 +213,6 @@ export class TakeoffComponent implements OnInit {
                 () => {
                     this.selectedProductsLoading = false;
                     this.GetProductProperties(this.selectedProducts[0]);
-                    this.selectedValueProperties.length = 0;
                 }
             );
         return false;
@@ -212,7 +220,7 @@ export class TakeoffComponent implements OnInit {
 
     GetProductProperties(product: IProduct) {
         this.selectedValueProperties.length = 0;
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.highlightedElements = [];
 
         this.displayedPropertySets.length = 0;
@@ -280,17 +288,6 @@ export class TakeoffComponent implements OnInit {
             this.AddPropertyToColumns(displayProperty);
         }
         this.GetGroupedPropertyCount();
-    }
-
-    onFilterPropertyDrop(e: DropEvent) {
-        this.selectedFilterProperties.push(e.dragData);
-    }
-
-    onFilterLabelClose(e: DisplayProperty) {
-        let index = this.selectedFilterProperties.indexOf(e, 0);
-        if (index > -1) {
-            this.selectedFilterProperties.splice(index, 1);
-        }
     }
 
     onValuePropertyDrop(e: DropEvent) {
@@ -390,72 +387,72 @@ export class TakeoffComponent implements OnInit {
 
     GetGroupedPropertyCount(): any {
 
-        this.listOfRows.length = 0;
+        this.listOfRows = [];
         this.highlightedElements = [];
 
         if (this.selectedValueProperties && this.selectedValueProperties.length !== 0) {
-            this.tableLoading = true;
-            const promise = this.ProcessData();
-            promise.then(() => {
-                this.tableLoading = false;
+            // Get the first column
+            let propertyArray = Products.GetGroupedList(this.selectedValueProperties[0], this.selectedProducts);
+
+            // Create the tree
+            let tree: ValueTree[] = [];
+
+            propertyArray.forEach(value => {
+                let filteredProducts = this.selectedProducts;
+                if (this.selectedValueProperties[0].groupingMode.mode === GroupingModeEnum.DontSummarize) {
+                    filteredProducts = Products.GetFilteredProducts(
+                        this.selectedProducts,
+                        this.selectedValueProperties[0].path,
+                        value
+                    );
+                }
+
+                let filteredProducts2 = Products.GetFilteredProducts(
+                    this.selectedProducts,
+                    this.selectedValueProperties[0].path,
+                    value
+                );
+
+                tree.push(new ValueTree(
+                    value,
+                    0,
+                    this.selectedValueProperties,
+                    filteredProducts
+                ));
             });
+
+            // Create the rows
+            let tableRows = [];
+            let hightlighted = [];
+
+            let i = 0;
+            let max = tree.reduce((a, b) => a + b.rows.length, 0);
+
+            tree.forEach(treeItem => {
+                treeItem.rows.forEach(row => {
+
+
+                    let color: Color = new Color(0, max, i);
+                    row['color'] = color.GetColorFromRange();
+
+                    let highlightedElement: IHighlightedElements = {
+                        color: row['color'],
+                        ids: treeItem.products.map(product => { return product.objectId })
+                    }
+
+                    hightlighted.push(highlightedElement);
+                    tableRows.push(row);
+                    i++;
+                });
+            });
+
+            this.highlightedElements = hightlighted;
+            this.listOfRows = tableRows;
         } else {
-            this.listOfRows.length = 0;
+            this.listOfRows = [];
         }
     }
 
-    async ProcessData(): Promise<void> {
-
-
-        // Get the first column
-        let propertyArray = Products.GetGroupedList(this.selectedValueProperties[0], this.selectedProducts);
-
-        // Create the tree
-        let tree: ValueTree[] = [];
-
-        propertyArray.forEach(value => {
-            let filteredProducts = Products.GetFilteredProducts(
-                this.selectedProducts,
-                this.selectedValueProperties[0].path,
-                value
-            );
-
-            tree.push(new ValueTree(
-                value,
-                0,
-                this.selectedValueProperties,
-                filteredProducts
-            ));
-        });
-
-        // Create the rows
-        let tableRows = [];
-        let hightlighted = [];
-
-        let i = 0;
-        let max = tree.reduce((a, b) => a + b.rows.length, 0);
-
-        tree.forEach(treeItem => {
-            treeItem.rows.forEach(row => {
-
-
-                let color: Color = new Color(0, max, i );
-                row['color'] = color.GetColorFromRange();
-
-                let highlightedElement: IHighlightedElements = {
-                    color: row['color'],
-                    ids: treeItem.products.map(product => {return product.objectId})
-                }
-
-                hightlighted.push(highlightedElement);
-                tableRows.push(row);
-                i++;
-            });
-        });
-
-        this.highlightedElements = hightlighted;
-        this.listOfRows = tableRows;
-    }
 
     trackByFn(index, model) {
         return model.id;
