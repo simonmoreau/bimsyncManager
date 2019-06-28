98676b4c14498ed27217ec0745fc7752a7d09528
Simon Moreau <simon@bim42.com>
2019-06-28 12:50:14 +0200

    develop routine to handle pagination

--- src/app/bimsync/bimsync.service.ts
+++ src/app/bimsync/bimsync.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { mergeMap, tap, catchError } from 'rxjs/operators';
+import { Observable, empty, of } from 'rxjs';
+import { mergeMap, tap, catchError, map, expand, concatMap } from 'rxjs/operators';
 
 import { IProject } from '../shared/models/bimsync.model';
 import { UserService } from '../user/user.service';
@@ -20,10 +20,42 @@ export class BimsyncService {
     private http: HttpClient) { }
 
   getProjects(): Observable<IProject[]> {
-    return this.http.get<IProject[]>(
-      this.apiUrl + 'projects?pageSize=1000',
+    return this.get(this.apiUrl + 'projects?pageSize=100').pipe(
+      map(response => response.body)
+    );
+  }
+
+  private get(url: string): Observable<HttpResponse<any>> {
+    return this.http.get<any>(
+      url,
       {
-        headers: new HttpHeaders().set('Content-Type', 'application/json')
-      });
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+        observe: 'response'
+      }).pipe(
+        expand(response => {
+          const next = this.next(response);
+          if (next) {
+            return this.get(next);
+          } else {
+            return empty();
+          }
+        }),
+        concatMap(response => {
+          return of(response);
+        })
+      );
+  }
+
+
+  private next(response: HttpResponse<any>): string | null {
+    let url: string | null = null;
+    const link = response.headers.get('link');
+    if (link) {
+      const match = link.match(/<([^>]+)>;\s*rel="next"/);
+      if (match) {
+        [, url] = match;
+      }
+    }
+    return url;
   }
 }
