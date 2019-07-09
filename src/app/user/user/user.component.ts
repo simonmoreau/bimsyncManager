35b7be94bc74c30d887ac13e829508b4ee495a5b
simonmoreau <simon@bim42.com>
2019-07-09 10:28:47 +0200

    get the bimsync user on login, not yet working

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../user.service';
 import { IUser } from '../../shared/models/user.model';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { BimsyncService } from 'src/app/bimsync/bimsync.service';
+import { tap, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user',
@@ -12,21 +14,44 @@ import { Router } from '@angular/router';
 export class UserComponent implements OnInit {
 
   user: IUser;
+  user$: Observable<IUser>;
   clientId: string;
   callbackUrl: string;
-
-  private apiUrl = 'https://binsyncfunction-dev.azurewebsites.net/api';
-  // private apiUrl = 'https://binsyncfunction-dev.azurewebsites.net/api';
+  avatarUrl: string;
 
   constructor(
     private router: Router,
-    private userService: UserService) {
-    this.userService.currentUser.subscribe(u => this.user = u);
-    this.clientId = this.userService.clientId;
-    this.callbackUrl = this.userService.url + '/callback';
+    private userService: UserService,
+    private bimsyncService: BimsyncService) {
+
   }
 
   ngOnInit() {
+
+    this.clientId = this.userService.clientId;
+    this.callbackUrl = this.userService.url + '/callback';
+    this.user$ = this.userService.currentUser;
+
+    this.user$.subscribe(
+      {
+        next: user => {
+          this.user = user;
+          this.bimsyncService.getCurrentUser().subscribe(
+            {
+              next: bimsyncUser => {
+                this.avatarUrl = bimsyncUser.avatarUrl;
+              },
+              error: error => this.logout(),
+              complete: () => console.log('completed')
+            }
+          );
+        },
+        error: error => console.log(error),
+        complete: () => {
+
+        }
+      }
+    );
   }
 
   logout() {
